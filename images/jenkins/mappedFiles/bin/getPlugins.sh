14965d06b43204f80e6f7fa89aa549d7a1d6017b
Deividas Sutkus <deividas.sutkus@qa.com>
2017-06-15 12:39:18 +0100

    Generate a JSON object of currently installed plugins.

--- images/jenkins/mappedFiles/bin/getPlugins.sh
+++ images/jenkins/mappedFiles/bin/getPlugins.sh
@@ -1,3 +1,8 @@
 #!/bin/bash
 
-find ${JENKINS_HOME}/plugins -maxdepth 1 -type d -exec cat {}/META-INF/MANIFEST.MF \; | grep -E 'Short-Name|Plugin-Version' | awk '{ print $2 }' | tr -d ' \r' | paste -d ':' - - | sort
+# Iterate over currently installed plugins
+# Create a map of plugin:version
+# Convert the map into a JSON object using jq
+find ${JENKINS_HOME}/plugins -maxdepth 1 -mindepth 1 -type d -exec cat {}/META-INF/MANIFEST.MF \; \
+  | grep -E 'Short-Name|Plugin-Version' | awk '{ print $2 }' | tr -d ' \r' | paste -d ':' - - | sort \
+  | jq -Rn '["plugin","version"] as $keys | (inputs | split(":")) as $plugins | [[$keys, $plugins] | transpose[] | {key:.[0],value:.[1]}] | from_entries'
