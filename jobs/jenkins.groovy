b7ed9b5f7db38cafaeafdb234c10b5e3a2ad34c6
Deividas Sutkus <deividas.sutkus@qa.com>
2017-06-26 09:55:04 +0100

    Deployment job for jenkins.

--- jobs/jenkins.groovy
+++ jobs/jenkins.groovy
@@ -1,4 +1,4 @@
-pipelineJob('jenkins') {
+pipelineJob('images/jenkins') {
     definition {
         cpsScm {
             scm {
@@ -14,5 +14,49 @@ pipelineJob('jenkins') {
     }
     configure {
        it / definition / lightweight(true)
+    }
+}
+
+pipelineJob('images/docker-slave') {
+    definition {
+        cpsScm {
+            scm {
+                git {
+                    branch('development')
+                    remote {
+                        url('https://github.com/Artiax/kubernetes.git')
+                    }
+                }
+            }
+            scriptPath('images/docker-slave/Jenkinsfile')
+        }
+    }
+    configure {
+       it / definition / lightweight(true)
+    }
+}
+
+pipelineJob('deployments/jenkins') {
+    parameters {
+        stringParam('TAG','default')
+    }
+    definition {
+        cps {
+            echo 'Spawning a slave for this job...'
+
+            node('docker') {
+                stage('Clone') {
+                    git branch: 'development', url: 'https://github.com/Artiax/kubernetes.git', changelog: false, poll: false
+                }
+
+                stage('Deploy') {
+                    dir('deployments') {
+                        sh 'sed -ri "s/(image:.*jenkins:)[a-z0-9\\.]*/\\1$TAG/" jenkins.yaml'
+                        sh 'kubectl apply --record -f jenkins.yaml'
+                    }
+                }
+            }
+            sandbox(true)
+        }
     }
 }
