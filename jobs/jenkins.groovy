b277f21f5d2fc1dd052611eaa02e66d036b43f4c
Deividas Sutkus <deividas.sutkus@qa.com>
2017-06-26 10:00:00 +0100

    Wrap the definition in script function.

--- jobs/jenkins.groovy
+++ jobs/jenkins.groovy
@@ -42,20 +42,22 @@ pipelineJob('deployments/jenkins') {
     }
     definition {
         cps {
-            echo 'Spawning a slave for this job...'
+            script("""
+                echo 'Spawning a slave for this job...'
 
-            node('docker') {
-                stage('Clone') {
-                    git branch: 'development', url: 'https://github.com/Artiax/kubernetes.git', changelog: false, poll: false
-                }
+                node('docker') {
+                    stage('Clone') {
+                        git branch: 'development', url: 'https://github.com/Artiax/kubernetes.git', changelog: false, poll: false
+                    }
 
-                stage('Deploy') {
-                    dir('deployments') {
-                        sh 'sed -ri "s/(image:.*jenkins:)[a-z0-9\\.]*/\\1$TAG/" jenkins.yaml'
-                        sh 'kubectl apply --record -f jenkins.yaml'
+                    stage('Deploy') {
+                        dir('deployments') {
+                            sh 'sed -ri "s/(image:.*jenkins:)[a-z0-9\\.]*/\\1$TAG/" jenkins.yaml'
+                            sh 'kubectl apply --record -f jenkins.yaml'
+                        }
                     }
                 }
-            }
+            """.stripIndent())
             sandbox(true)
         }
     }
