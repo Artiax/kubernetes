139d079a52e133f80169e1afaffcfc4daaa7ce7a
Deividas Sutkus <deividas.sutkus@qa.com>
2017-07-06 17:29:11 +0100

    Trying to template the jobs.

---
+++ jobs/images.groovy
@@ -0,0 +1,24 @@
+[
+  [name: 'jenkins', repository: 'https://github.com/Artiax/kubernetes.git', branch: 'development'],
+  [name: 'docker-slave', repository: 'https://github.com/Artiax/kubernetes.git', branch: 'development'],
+  [name: 'httpd', repository: 'https://github.com/Artiax/kubernetes.git', branch: 'development'],
+].each { Map image ->
+  pipelineJob("images/${image.name}") {
+      definition {
+          cpsScm {
+              scm {
+                  git {
+                      branch("${image.branch}")
+                      remote {
+                          url("${image.repository}")
+                      }
+                  }
+              }
+              scriptPath("images/${image.name}/Jenkinsfile")
+          }
+      }
+      configure {
+         it / definition / lightweight(true)
+      }
+  }
+}
